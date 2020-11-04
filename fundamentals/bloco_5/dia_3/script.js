fe0788d6ef54ea753c10a7f80689fcd2a0bdf7fe
emassaki <massakihirayama@gmail.com>
2020-11-04 15:09:59 -0300

    Adding exercise 1 - add calendar days with JS and add multiple classes

---
+++ fundamentals/bloco_5/dia_3/script.js
@@ -0,0 +1,37 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+function createDaysOfTheMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.getElementById('days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const day = dezDaysList[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = day;
+    dayListItem.className = 'day';
+    if (day === 24 || day === 25 || day === 31) {
+      dayListItem.className = 'day holiday';
+    }
+    if (day === 4 || day === 11 || day === 18 || day === 25) {
+      dayListItem.className = 'day friday';
+    }
+
+    daysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheMonth();
