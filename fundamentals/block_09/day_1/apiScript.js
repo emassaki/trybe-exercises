fa4458649648ae8af2e3847105a8f028e5dc7c28
emassaki <massakihirayama@gmail.com>
2020-12-09 14:55:14 -0300

    Added exercise 3

--- fundamentals/block_09/day_1/apiScript.js
+++ fundamentals/block_09/day_1/apiScript.js
@@ -7,25 +7,31 @@ const fetchJoke = () => {
   };
 
   fetch(API_URL, myObject)
-  .then(response => response.json())
-  .then(data => {
-    // Exercise 1 - add a joke to the page
-    document.getElementById('jokeContainer').innerHTML = data.joke;
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      // Exercise 1 - add a joke to the page
+      document.getElementById('jokeContainer').innerHTML = data.joke;
+    });
 
   // Exercise 2 - check if the promise is resolved or rejected
   const promise = new Promise((resolve, reject) => {
     const array = [];
     for (let index = 0; index < 10; index += 1) {
-      array.push(Math.ceil(Math.random() * 50))
+      array.push(Math.ceil(Math.random() * 50));
     }
-    const sumOfSqrdArray = array.map(number => number ** 2).reduce((total, curr) => total += curr)
+    const sumOfSqrdArray = array
+      .map((number) => number ** 2)
+      .reduce((total, curr) => (total += curr));
     if (sumOfSqrdArray >= 8000) {
-      return reject(sumOfSqrdArray)
+      return reject(sumOfSqrdArray);
     }
-    resolve(sumOfSqrdArray)
-  }).then(number => console.log(`A soma dos quadrados do array, ${number}, é menor que 8000`)).catch(number => console.log(`A soma dos quadrados do array, ${number}, é maior que 8000`))
+    resolve(sumOfSqrdArray);
+  })
+    // Exercise 3 - log an array with the division result of 2, 3, 5 and 10
+    .then((number) =>
+      console.log([2, 3, 5, 10].map((quotient) => Math.round((number / quotient) * 100) / 100))
+    )
+    .catch((number) => console.log(`A soma dos quadrados do array, ${number}, é maior que 8000`));
 };
-
 
 window.onload = () => fetchJoke();
