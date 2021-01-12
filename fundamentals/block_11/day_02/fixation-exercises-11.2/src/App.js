1fd5ffd65915a2efa2247b4c22b7262895245b7c
emassaki <massakihirayama@gmail.com>
2021-01-12 19:27:20 -0300

    Added pokedex exercise and fixations exercises of day 11.2

---
+++ fundamentals/block_11/day_02/fixation-exercises-11.2/src/App.js
@@ -0,0 +1,10 @@
+import Image from './Image'
+import './App.css';
+
+function App() {
+  return (
+    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
+  );
+}
+
+export default App;
