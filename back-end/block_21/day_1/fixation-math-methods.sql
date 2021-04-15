e2d434da6809229d01a7a0a77b488008c2bfbe41
emassaki <massakihirayama@gmail.com>
2021-04-15 15:38:20 -0300

    Added exercises

---
+++ back-end/block_21/day_1/fixation-math-methods.sql
@@ -0,0 +1,7 @@
+SELECT 15 AS number, IF (15 MOD 2 = 0, 'Par', 'Ímpar') AS 'Par ou Ímpar';
+SELECT 220 AS max_capacity, 220 DIV 12 AS num_of_groups, 220 MOD 12 AS empty_spaces;
+
+SELECT ROUND(15 + (RAND() * 5));
+SELECT ROUND(15.75, 5);
+SELECT FLOOR(39.494);
+SELECT CEIL(85.234);
